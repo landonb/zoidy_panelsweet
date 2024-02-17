1f628151c0ed6766a808d817804822ee98288281
Landon Bouma <landonb@users.noreply.github.com>
2024-02-16 22:37:22 -0600

    Improve: Add user service to restore (fix) mate-panel layout on unlock
    - For whatever reason, after locking (the screensaver) and logging on
      (unlocking), sometimes the two bottom panels have swapped positions.
    
      This service restores the mate-panel dconf, undoing the change.

---
+++ templates/user/restore-mate-panels.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+# REFER/2024-02-16: As inspired by (and mostly copied from):
+# https://unix.stackexchange.com/questions/28181/how-to-run-a-script-on-screen-lock-unlock/368632#368632
+# - REFER: If you have multiple DBus lines, see:
+#   - *Discover a DBUS Session Deterministically*
+#     https://gist.github.com/naftulikay/f4c229b3c71fff9ac2102a0e03bd756f
+
+# ***
+
+# The screensaver name, e.g.:
+#   org.gnome.ScreenSaver
+#   org.freedesktop.ScreenSaver
+SCREENSAVER_ID="org.mate.ScreenSaver"
+DCONF_DIR="/org/mate/panel/"
+DCONF_DUMP="${HOME}/.local/share/zoidy_panelsweet/dconf--org-mate-panel.dump"
+
+# ***
+
+# Good practice: exit if unset variable used.
+set -o nounset
+
+# Lock file path
+pidfile=/tmp/restore-mate-panels.sh.pid
+# Log file path
+logfile=/tmp/restore-mate-panels.sh.log
+
+# Cleanup trap
+cleanup() {
+  # Remove the lock file
+  rm -f ${pidfile}
+  # Reset kernel signal catching
+  trap - INT TERM EXIT
+  # Stop the daemon
+  exit
+}
+
+# Simple logging mechanism
+log() {
+  echo "$(date +%Y-%m-%d\ %X) -- ${USER} -- \"$@\"" >> ${logfile}
+}
+
+# Exit if lock file exists
+if [ -e "${pidfile}" ]; then
+  log "$0 already running..."
+
+  exit
+fi
+
+# Call cleanup() if e.g. killed
+trap cleanup INT TERM EXIT
+
+log "daemon started..."
+
+# Create lock file with own PID inside
+echo $$ > ${pidfile}
+
+# Usually `dbus-daemon` address can be guessed (`-s` returns 1st PID found)
+# - Pipe to `xargs -0` to suppress message:
+#   bash: warning: command substitution: ignored null byte in input
+export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pidof -s dbus-daemon)/environ | xargs -0)
+
+# DBus watch expression
+expr="type=signal,interface=${SCREENSAVER_ID}"
+
+prev_line=""
+
+dbus-monitor --address "${DBUS_SESSION_BUS_ADDRESS}" "${expr}" | \
+  while read line; do
+    if echo "${prev_line}" | grep -q "; member=ActiveChanged$"; then
+      case "${line}" in
+        *"boolean true"*)
+          log "session locked"
+          ;;
+        *"boolean false"*)
+          # The meat of what this daemon does:
+          # - Restore the panel arrangement.
+          cat "${DCONF_DUMP}" \
+            | dconf load ${DCONF_DIR}
+          log "session unlocked — restored dconf ${DCONF_DIR}"
+          ;;
+      esac
+    fi
+    prev_line="${line}"
+  done
+
+# Avoid leaving orphaned lock file when the loop ends (e.g. dbus dies)
+cleanup
+
